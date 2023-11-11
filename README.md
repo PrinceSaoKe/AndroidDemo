a383fda54e82fe3fe932a3d1f017f9a6c2051532
Prince <2596818595@qq.com>
2023-11-11 17:46:09 +0800

    添加string.xml，README

---
+++ README.md
@@ -0,0 +1,5 @@
+# 遇到的问题
+
+* 使用DataBinding，传入图片的resourceId无法显示图片
+* 使用intent传递对象
+* intent传递对象是传递其拷贝而非对象本身，在新的Activity中修改传递的对象无法影响到上个Activity中实例化的对象
--- app/src/main/java/com/saoke/androiddemo/DetailActivity.kt
+++ app/src/main/java/com/saoke/androiddemo/DetailActivity.kt
@@ -1,7 +1,8 @@
 package com.saoke.androiddemo
 
 import android.os.Bundle
-import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.ComponentActivity
 import androidx.databinding.DataBindingUtil
 import com.saoke.androiddemo.databinding.ActivityDetailBinding
@@ -10,15 +11,21 @@ class DetailActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val intent = intent
-//        原本打算用序列化传递对象的，但是我手机的API版本太低
+//        原本打算用序列化直接传递对象的，但是我手机的API版本太低不支持这种写法
 //        val up = intent.getSerializableExtra("up", Up::class.java)
         val name = intent.getStringExtra("name")
         val avatarResourceId = intent.getIntExtra("avatarResourceId", 0)
         val fansNumber = intent.getIntExtra("fansNumber", 0)
         val binding: ActivityDetailBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_detail)
+
         binding.name = name
-        binding.avatarResourceId = avatarResourceId
         binding.fansNumber = fansNumber
+//        不知道为什么对头像的resourceId赋值，无法显示图片，只好改用findViewById
+//        binding.avatarResourceId = avatarResourceId
+        val avatar = findViewById<ImageView>(R.id.avatar)
+        avatar.setImageResource(avatarResourceId)
+
+        val followButton = findViewById<Button>(R.id.followButton)
     }
 }
