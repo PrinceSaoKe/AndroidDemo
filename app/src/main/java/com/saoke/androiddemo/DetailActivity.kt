bf34457678c54bf0943338b14946dbee92cc1e9b
Prince <2596818595@qq.com>
2023-11-15 23:35:48 +0800

    实现关注按钮文字刷新

--- app/src/main/java/com/saoke/androiddemo/DetailActivity.kt
+++ app/src/main/java/com/saoke/androiddemo/DetailActivity.kt
@@ -2,11 +2,8 @@ package com.saoke.androiddemo
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.databinding.DataBindingUtil
 import com.saoke.androiddemo.databinding.ActivityDetailBinding
 
 class DetailActivity : ComponentActivity() {
@@ -14,31 +11,37 @@ class DetailActivity : ComponentActivity() {
     private var name: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        if (savedInstanceState != null) {
+            followAction = savedInstanceState.getBoolean("followAction")
+        }
         val intent = intent
-//        原本打算用序列化直接传递对象的，但是我手机的API版本太低不支持这种写法
+//        原本打算用序列化直接传递对象的，但是好像我手机的API版本太低不支持这种写法？
 //        val up = intent.getSerializableExtra("up", Up::class.java)
         name = intent.getStringExtra("name")!!
         val avatarResourceId = intent.getIntExtra("avatarResourceId", 0)
         val fansNumber = intent.getIntExtra("fansNumber", 0)
         super.onCreate(savedInstanceState)
-        val binding: ActivityDetailBinding =
-            DataBindingUtil.setContentView(this, R.layout.activity_detail)
+//        val binding: ActivityDetailBinding =
+//            DataBindingUtil.setContentView(this, R.layout.activity_detail)
 
-        binding.name = name
-        binding.fansNumber = fansNumber
+        val myBinding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(myBinding.root)
+        myBinding.followButton.text = if (followAction) "取消关注" else "关注"
+
+        myBinding.name = name
+        myBinding.fansNumber = fansNumber
 //        不知道为什么对头像的resourceId赋值，无法显示图片，只好改用findViewById
 //        binding.avatarResourceId = avatarResourceId
-        val avatar = findViewById<ImageView>(R.id.avatar)
-        avatar.setImageResource(avatarResourceId)
+        myBinding.avatar.setImageResource(avatarResourceId)
 
-        val followButton = findViewById<Button>(R.id.followButton)
-        followButton.setOnClickListener {
+        myBinding.followButton.setOnClickListener {
             if (followAction) {
                 Toast.makeText(this, "已取消关注", Toast.LENGTH_SHORT).show()
             } else {
                 Toast.makeText(this, "已关注", Toast.LENGTH_SHORT).show()
             }
             followAction = !followAction
+            recreate()
         }
     }
 
@@ -49,5 +52,10 @@ class DetailActivity : ComponentActivity() {
             setResult(RESULT_OK, intent)
         }
         finish()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean("followAction", followAction)
     }
 }
